c8407014ebc0f048033484ed984d3159b6c330cd
Hugo Gonzaga Sánchez <82997251+TheRichKidd@users.noreply.github.com>
2022-05-05 15:33:07 -0500

    Delete input Function.py

--- input Function.py
+++
@@ -1,9 +0,0 @@
-'''
-Instructions:
-Write a program that prints the number of characters in a user's name.
-You might need to Google for a function that calculates the length of 
-a string.
-'''
-s = str(input('What your name? '))
-print(len(s))
-
